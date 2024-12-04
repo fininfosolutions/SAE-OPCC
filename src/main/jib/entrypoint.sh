cf2db667e689bb8d8c9f79540a658a1734283400
Issra BOUAFIA <ibouafia@fininfosolutions.com>
2024-12-04 13:15:25 +0000

    A mis à jour entrypoint.sh

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.fininfo.saeopcc.SaeOPCCApplication"  "$@"
+exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.fininfo.saeopcc.src/main/java/com/fininfo/saeopcc/SaeOpccApplication.java"  "$@"
