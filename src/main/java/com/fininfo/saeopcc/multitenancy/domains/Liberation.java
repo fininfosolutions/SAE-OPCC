8b9182bb10497e2847c6a3b5c25e6d5336a6df42
aabaccar <aabaccar@fininfosolutions.com>
2025-01-09 14:31:30 +0100

    fix bigdecimal fields

--- src/main/java/com/fininfo/saeopcc/multitenancy/domains/Liberation.java
+++ src/main/java/com/fininfo/saeopcc/multitenancy/domains/Liberation.java
@@ -5,6 +5,8 @@ import com.fininfo.saeopcc.shared.domains.AbstractAuditingEntity;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -33,19 +35,20 @@ public class Liberation extends AbstractAuditingEntity implements Serializable {
   private String description;
   private String reference;
   private String message;
-
+  @Column(precision = 21, scale = 4)
   private BigDecimal percentage;
-
-  private LocalDate releasedAmount;
-
+  @Column(precision = 21, scale = 4)
+  private BigDecimal releasedAmount;
+  @Column(precision = 21, scale = 4)
   private BigDecimal releasedQuantity;
+  @Column(precision = 21, scale = 4)
   private BigDecimal remainingAmount;
 
   private LocalDate liberationDate;
 
   @Enumerated(EnumType.STRING)
   private LiberationStatus status;
-
+  @Column(precision = 21, scale = 4)
   private BigDecimal remainingQuantity;
 
   @ManyToOne @EqualsAndHashCode.Exclude private GlobalLiberation liberationEvent;
