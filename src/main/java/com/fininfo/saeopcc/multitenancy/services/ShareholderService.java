1f1bd45f0ebb54072ced0832e36a1d1bbb8f823c
aabaccar <aabaccar@fininfosolutions.com>
2025-03-28 11:52:48 +0100

    fix synchro shareholder

--- src/main/java/com/fininfo/saeopcc/multitenancy/services/ShareholderService.java
+++ src/main/java/com/fininfo/saeopcc/multitenancy/services/ShareholderService.java
@@ -14,7 +14,6 @@ import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
